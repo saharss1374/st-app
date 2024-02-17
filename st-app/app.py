1e42348a0e1181d0d91b083fe7a128ba7891623a
saharss1374 <143895724+saharss1374@users.noreply.github.com>
2024-02-17 12:05:54 +0330

    Add files via upload

---
+++ st-app/app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+import pickle
+import warnings
+warnings.filterwarnings("ignore")
+from absenteeism_module import *
+
+def Input_Output():
+    data=st.file_uploader("upload file",type={"csv","txt"})
+    if data is not None:
+        df=pd.read_csv(data)
+        model=absenteeism_model('model','scaler')
+        model.load_and_clean_data('Absenteeism_new_data.csv')
+    result=""
+    if st.button("click here to predict"):
+        result=model.predicted_outputs()
+        st.balloons()
+    st.success("the output is as follows: ")
+    st.write(result)
+
+if __name__=='__main__':
+   Input_Output()    
